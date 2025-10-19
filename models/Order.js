a5a83a24a940124f989b3451a481a91490b34959
Ahmed <bouahmed530@gmail.com>
2025-10-19 09:08:51 +0100

    made email optional in the chechout

--- models/Order.js
+++ models/Order.js
@@ -17,7 +17,7 @@ const orderSchema = new mongoose.Schema(
     ],
     shippingInfo: {
       fullName: { type: String, required: true },
-      email: { type: String, required: true },
+      email: { type: String, required: false },
       phone: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
